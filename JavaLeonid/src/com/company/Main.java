5ecb9f4d9d26344ab60a0075c180cc4cd6b2c8d2
ArturPromon <artur.shovkunenko@gmail.com>
2022-09-21 20:28:12 +0300

    created first project

--- JavaLeonid/src/com/company/Main.java
+++ JavaLeonid/src/com/company/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello My name is Artur");
         System.out.print("Hello My name is Artur");
-        System.out.print("Hello My name is Artur");
-        System.out.print("Hello My name is Artur");
     }
 }
