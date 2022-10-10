d1ba9a3f0713e376c4fec3ba0fe02e6c98d82fc9
ArturPromon <artur.shovkunenko@gmail.com>
2022-10-10 18:45:17 +0300

    While cycle

---
+++ JavaLeonid/src/com/company/Java10.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Java10 {
+    public static void main(String[] args) {
+//        boolean isTrue = true;
+//        int i = 0;
+//        while(isTrue){
+//            System.out.println("Hello world!");
+//            i++;
+//            if(i > 5){
+//                isTrue = false;
+//            }
+//        }
+
+//        int i = 0;
+//        while(true){
+//            i++;
+//            System.out.println("Hello world!");
+//            if(i> 5){
+//                break;
+//            }
+//        }
+        int i = 0;
+        while(i<5){
+            i++;
+            System.out.println("Hello world");
+        }
+    }
+}
